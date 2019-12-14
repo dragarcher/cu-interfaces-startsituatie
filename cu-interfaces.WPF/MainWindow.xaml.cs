5a3981d8cacad02a8bd11bc65690b1e0bf707895
Dries Deboosere <dries.deboosere@howest.be>
2019-12-14 09:42:44 +0100

    Add project files.

---
+++ cu-interfaces.WPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace cu_interfaces.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnTvWoonkamerPower_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnSmartLampGangPower_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnRadioKeuken_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnTvWoonKamerVolumeDown_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnTvWoonKamerVolumeUp_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnRadioKeukenVolumeDown_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnRadioKeukenVolumeUp_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void btnCheckConnections_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
